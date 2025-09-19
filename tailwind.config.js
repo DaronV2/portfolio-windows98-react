09b2cccd344ac5a78dec3119ce9b743bf2a47f7a
DaronV2 <darkenciel31@gmail.com>
2025-09-19 21:18:37 +0200

    removing useless custom css tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,12 +15,7 @@ module.exports = {
         "buttonColor" : "rgb(172, 172, 172)",
         "black" : "black",
         "white" : "white"
-      },
-      boxShadow: {
-        'custom-inset': 'inset 2px 2px rgb(206, 206, 206), inset -2px -2px gray',
-        'custom-inset-type' : "inset 2px 2px rgb(40, 40, 40),inset -2px -2px rgb(226, 226, 226)",
-        'custom-inset-button' : "inset 2px 2px rgb(236, 236, 236),inset -2px -2px rgb(0, 0, 0)"
-      },
+      }
     },
   },
   plugins: [],
