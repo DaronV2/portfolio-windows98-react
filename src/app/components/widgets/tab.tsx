6dbc67179cf5613cf5aa8478c4caa29530af941e
DaronV2 <darkenciel31@gmail.com>
2025-10-03 18:09:57 +0200

    small style changes

--- src/app/components/widgets/tab.tsx
+++ src/app/components/widgets/tab.tsx
@@ -3,13 +3,15 @@ import { ReactNode } from "react";
 export default function Tab(title : string, children : ReactNode){
     return (
         <div className="p-1 m-1 bg-[#acacac] border-2 border-[#929292] shadow-custom-inset text-center font-pixelify text-azure justify-center flex-col ">  
-            <div className="title bg-gradient-to-r from-[#112772] to-[#aac2df] flex items-center justify-between px-1 text-xs">
+            <div className="title bg-gradient-to-r from-[#112772] to-[#aac2df] flex items-center justify-between px-1 text-xl mb-0.5">
                 <p className="font-pixelify flex-1 truncate text-left">{title}</p>
-                <div className="closeBtn aspect-square max-h-4 flex justify-center items-center text-center cursor-pointer bg-[#acacac] m-0.5 text-black shadow-(--custom-inset-button) hover:bg-[#4d4d4d] hover:shadow-(--custom-inset-type) hover:text-white hover:scale-[1.1] hover:transition-all hover:duration-200 hover:ease-out font-geist min-h-4">
+                <div className="closeBtn aspect-square max-h-4  text-xs flex justify-center items-center text-center cursor-pointer bg-[#acacac] m-0.5 text-black shadow-(--custom-inset-button) hover:bg-[#4d4d4d] hover:shadow-(--custom-inset-type) hover:text-white hover:scale-[1.1] hover:transition-all hover:duration-200 hover:ease-out font-geist min-h-4">
                     X
                 </div>
             </div>
-            {children}
+            <div className=" text-2xl">
+                {children}
+            </div>
         </div>
     );
 }
