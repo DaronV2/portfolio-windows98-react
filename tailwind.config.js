46b62c8167e2b61e86ccc8dded514b221f690cee
DaronV2 <darkenciel31@gmail.com>
2025-10-03 18:10:37 +0200

    Solving / clearing css import bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"
-    ,"./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      fontFamily : {
+      fontFamily: {
         pixelify: ["var(--font-pixelify)", "sans-serif"],
-        geist: ['var(--font-geist-sans)', 'sans-serif'],
-        geistMono: ['var(--font-geist-mono)', 'monospace'],
+        geist: ["var(--font-geist-sans)", "sans-serif"],
+        geistMono: ["var(--font-geist-mono)", "monospace"],
+        chicle: ["var(--font-chicle)", "sans-serif"],
       },
-      colors : {
-        "buttonColor" : "rgb(172, 172, 172)",
-        "black" : "black",
-        "white" : "white"
-      }
+      colors: {
+        button: "rgb(172,172,172)",
+      },
     },
   },
   plugins: [],
