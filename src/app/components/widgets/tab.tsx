94caeae6a76cd241c61a5b995a0c0f587428ab4e
DaronV2 <darkenciel31@gmail.com>
2026-04-22 16:56:53 +0200

    adding style to tab

--- src/app/components/widgets/tab.tsx
+++ src/app/components/widgets/tab.tsx
@@ -5,11 +5,12 @@ interface TabProps {
   title: string;
   children: React.ReactNode;
   onCloseButton?: () => void;
+  style? : string;
 }
 
-export default function Tab({ title, children, onCloseButton }: TabProps){
+export default function Tab({ title, children, onCloseButton, style }: TabProps){
     return (
-        <div id={`tab-${title}`} className={` p-1 m-1 bg-[#acacac] border-2 border-[#929292] shadow-custom-inset text-center font-pixelify text-azure justify-center flex-col `}>  
+        <div id={`tab-${title}`} className={` p-1 m-1 bg-[#acacac] border-2 border-[#929292] shadow-custom-inset text-center font-pixelify text-azure justify-center flex-col ${style}`}>  
             <div className="title bg-gradient-to-r from-[#112772] to-[#aac2df] flex items-center justify-between px-1 text-xl mb-0.5">
                 <p className="font-pixelify flex-1 truncate text-left">{title}</p>
                 <Button isSquare hover onClickFunction={onCloseButton}>
